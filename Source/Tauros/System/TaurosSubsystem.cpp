f5074e62a3570e8aa00ed437ae38e03f0c9010ff
tarnishablec <tarnishablec@outlook.com>
2025-08-12 05:06:45 +0800

    External Login

--- Source/Tauros/System/TaurosSubsystem.cpp
+++ Source/Tauros/System/TaurosSubsystem.cpp
@@ -18,7 +18,7 @@ void UTaurosSubsystem::Deinitialize()
 {
     Super::Deinitialize();
     FTSTicker::GetCoreTicker().RemoveTicker(TickHandle);
-    OnlineServicesInfo->Reset();
+    OnlineServicesInfo.Reset();
 }
 
 void UTaurosSubsystem::Initialize(FSubsystemCollectionBase& Collection)
@@ -39,60 +39,16 @@ void UTaurosSubsystem::Initialize(FSubsystemCollectionBase& Collection)
         }));
 
 
-        GameInstance->OnLocalPlayerAddedEvent.AddLambda([this](const ULocalPlayer* LocalPlayer)
+        GameInstance->OnLocalPlayerAddedEvent.AddLambda([this](ULocalPlayer* LocalPlayer)
         {
-            const auto Oss = Online::GetSubsystem(GetWorld(),"Steam");
-
-
-            if (!Oss) { return; }
-
-            const auto Identity = Oss->GetIdentityInterface();
-
-            if (!Identity) { return; }
-
-            const auto Delegate = IOnlineIdentity::FOnGetLinkedAccountAuthTokenCompleteDelegate::CreateLambda(
-                [LocalPlayer,this](int32 LocalUserNum, bool bWasSuccessful, const FExternalAuthToken& AuthToken)
-                {
-                    const auto PlatformUserId = LocalPlayer->GetPlatformUserId();
-
-                    UE::Online::FAuthLogin::Params Params{
-                        .PlatformUserId = PlatformUserId,
-                        .CredentialsType = UE::Online::LoginCredentialsType::ExternalAuth,
-                    };
-
-
-                    UE::Online::FExternalAuthToken CredentialsToken{
-                        .Type = LexToString(EOS_EExternalCredentialType::EOS_ECT_STEAM_SESSION_TICKET),
-                        .Data = AuthToken.TokenString
-
-                    };
-
-                    Params.CredentialsToken.Emplace<UE::Online::FExternalAuthToken>(CredentialsToken);
-
-
-                    auto LoginHandle = OnlineServicesInfo->AuthInterface->Login(MoveTemp(Params)).OnComplete(
-                        [](const UE::Online::TOnlineResult<UE::Online::FAuthLogin>& Result)
-                        {
-                            if (Result.IsOk())
-                            {
-                                check(0)
-                                const auto AccountInfo = Result.GetOkValue().AccountInfo;
-                                if (AccountInfo->AccountId)
-                                {
-                                    UE_LOG(LogTemp, Display, TEXT("Login successfully %d"),
-                                           AccountInfo->PlatformUserId.GetInternalId());
-                                }
-                            }
-                            else
-                            {
-                                check(0)
-                            }
-                        });
-                });
-
-            Identity->GetLinkedAccountAuthToken(0, "WebAPI:epiconlineservices", Delegate);
-
-            // const auto Ticket = Identity->GetAuthToken(0);
+            if (GetGameInstance()->GetFirstGamePlayer() == LocalPlayer)
+            {
+                EosExternalLogin(
+                    LocalPlayer,
+                    EOS_EExternalAccountType::EOS_EAT_STEAM,
+                    EOS_EExternalCredentialType::EOS_ECT_STEAM_SESSION_TICKET
+                );
+            }
         });
     }
 }
@@ -104,21 +60,74 @@ bool UTaurosSubsystem::ShouldCreateSubsystem(UObject* Outer) const
 
 void UTaurosSubsystem::InitializeOnlineServices()
 {
-    OnlineServicesInfo = new FOnlineServicesInfo();
-
-    // 初始化服务指针
-    OnlineServicesInfo->OnlineServices = UE::Online::GetServices();
-    check(OnlineServicesInfo->OnlineServices.IsValid());
-
-    // 验证服务类型
-    OnlineServicesInfo->OnlineServicesType = OnlineServicesInfo->OnlineServices->GetServicesProvider();
-    if (!OnlineServicesInfo->OnlineServices.IsValid())
+    OnlineServicesInfo.OnlineServices = UE::Online::GetServices();
+    if (!OnlineServicesInfo.OnlineServices.IsValid())
     {
         UE_LOG(LogTemp, Error, TEXT("Error: Failed to initialize services."));
         return;
     }
+}
 
-    // 初始化接口指针
-    OnlineServicesInfo->AuthInterface = OnlineServicesInfo->OnlineServices->GetAuthInterface();
-    OnlineServicesInfo->ConnectivityInterface = OnlineServicesInfo->OnlineServices->GetConnectivityInterface();
+void UTaurosSubsystem::EosExternalLogin(const ULocalPlayer* LocalPlayer, const EOS_EExternalAccountType AccountType,
+                                        EOS_EExternalCredentialType CredentialType)
+{
+    const auto Oss = Online::GetSubsystem(GetWorld(), LexToString(AccountType));
+
+    if (!Oss) { return; }
+
+    const auto Identity = Oss->GetIdentityInterface();
+
+    if (!Identity) { return; }
+
+    const auto Delegate = IOnlineIdentity::FOnGetLinkedAccountAuthTokenCompleteDelegate::CreateLambda(
+        [this,CredentialType]
+    (const int32 LocalUserNum, const bool bWasSuccessful, const FExternalAuthToken& AuthToken)
+        {
+            if (!bWasSuccessful)
+            {
+                return;
+            }
+
+            const auto PlatformUserId = GetGameInstance()->GetLocalPlayerByIndex(LocalUserNum)->
+                                                           GetPlatformUserId();
+
+            UE::Online::FAuthLogin::Params Params{
+                .PlatformUserId = PlatformUserId,
+                .CredentialsType = UE::Online::LoginCredentialsType::ExternalAuth,
+            };
+
+
+            UE::Online::FExternalAuthToken CredentialsToken{
+                .Type = LexToString(CredentialType),
+                .Data = AuthToken.TokenString
+
+            };
+
+            Params.CredentialsToken.Emplace<UE::Online::FExternalAuthToken>(CredentialsToken);
+
+
+            OnlineServicesInfo.OnlineServices->GetAuthInterface()->Login(MoveTemp(Params)).OnComplete([]
+            (const UE::Online::TOnlineResult<UE::Online::FAuthLogin>& Result)
+                {
+                    if (Result.IsOk())
+                    {
+                        check(0)
+                        const auto AccountInfo = Result.GetOkValue().
+                                                        AccountInfo;
+                        if (AccountInfo->AccountId)
+                        {
+                            UE_LOG(LogTemp, Display, TEXT("Login successfully %d"),
+                                   AccountInfo->PlatformUserId.GetInternalId());
+                        }
+                    }
+                    else
+                    {
+                        check(0)
+                    }
+                });
+        });
+
+    Identity->GetLinkedAccountAuthToken(LocalPlayer->GetLocalPlayerIndex(),
+                                        "WebAPI:epiconlineservices",
+                                        Delegate);
 }
--- Source/Tauros/System/TaurosSubsystem.h
+++ Source/Tauros/System/TaurosSubsystem.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include <eos_common.h>
+
 #include "CoreMinimal.h"
 #include "Online/OnlineServices.h"
 #include "Subsystems/GameInstanceSubsystem.h"
@@ -22,6 +24,12 @@ public:
 
     void InitializeOnlineServices();
 
+    //
+
+    void EosExternalLogin(const ULocalPlayer* LocalPlayer,
+                          EOS_EExternalAccountType AccountType,
+                          EOS_EExternalCredentialType CredentialType);
+
 protected:
     FTSTicker::FDelegateHandle TickHandle;
 
@@ -29,28 +37,17 @@ protected:
     // UE::Online::TOnlineAsyncOpHandle<UE::Online::FAuthLogin> LoginHandle;
 
 
+    UE::Online::IOnlineServicesPtr OnlineServices;
+
     struct FOnlineServicesInfo
     {
-        /** 在线服务指针 - 通过此指针访问接口 */
         UE::Online::IOnlineServicesPtr OnlineServices = nullptr;
 
-        /** Auth接口 */
-        UE::Online::IAuthPtr AuthInterface = nullptr;
-
-        /** Connect接口 */
-        UE::Online::IConnectivityPtr ConnectivityInterface = nullptr;
-
-        /** 在线服务实现 */
-        UE::Online::EOnlineServices OnlineServicesType = UE::Online::EOnlineServices::None;
-
-        /** 将结构体重置为初始设置 */
         void Reset()
         {
             OnlineServices.Reset();
-            AuthInterface.Reset();
-            OnlineServicesType = UE::Online::EOnlineServices::None;
         }
     };
 
-    FOnlineServicesInfo* OnlineServicesInfo = nullptr;
+    FOnlineServicesInfo OnlineServicesInfo;
 };
